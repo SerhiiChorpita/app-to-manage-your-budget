5d44dafd163e4ddb03611933864fb40d62e6e15c
SerhiiChorpita <sergiochorpita@gmail.com>
2022-12-12 21:54:02 +0100

    created header&footer/working on style them

--- app-to-manage-your-budget/src/app/app.module.ts
+++ app-to-manage-your-budget/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header/header.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login/login.component';
+import { ContentComponent } from './components/content/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    LoginComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
